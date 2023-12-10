5a603a7f0b52050f6e630214138d1014e692bd6d
daren-tju <147412128+darendidar@users.noreply.github.com>
2023-12-10 15:28:12 +0800

    Update core.jl

--- src/core.jl
+++ src/core.jl
@@ -1,4 +1,4 @@
-import Base:  + ,-,*,/ ,^,sin,cos# 导入加减乘除和幂运算运算符
+import Base: +, -, *, /, ^, sin, cos, tanh,reshape,transpose# 导入需要重载的函数
 
 mutable struct Var
     data#用来存储数据
@@ -8,45 +8,59 @@ mutable struct Var
     generation#辈分
     name#起名
 end
-Var() = Var(nothing, nothing, nothing, nothing, 1,nothing)
-Var(x::AbstractArray) = Var(x, nothing, nothing, nothing, 1,nothing)#只能传入数组
+Var() = Var(nothing, nothing, nothing, nothing, 1, nothing)
+Var(x::AbstractArray) = Var(x, nothing, nothing, nothing, 1, nothing)#只能传入数组
 
 #x^2函数
 square(x) = x .^ 2
 struct Square end
-Square(x::Var) = Var(square(x.data), [x], Square, nothing, x.generation + 1,nothing)
+Square(x::Var) = Var(square(x.data), [x], Square, nothing, x.generation + 1, nothing)
 
 #加法
 struct Add end
-Add(a::Var, b::Var) = Var(a.data .+ b.data, [a, b], Add, nothing, max(a.generation, b.generation) + 1,nothing)
+Add(a::Var, b::Var) = Var(a.data .+ b.data, [a, b], Add, nothing, max(a.generation, b.generation) + 1, nothing)
 
 #减法
 struct Neg end
-Neg(x::Var) = Var(x.data .* -1, [x], Neg, nothing, x.generation + 1,nothing)
+Neg(x::Var) = Var(x.data .* -1, [x], Neg, nothing, x.generation + 1, nothing)
 
 #乘法
 struct Mul end
-Mul(a::Var, b::Var) = Var(a.data .* b.data, [a, b], Mul, nothing, max(a.generation, b.generation) + 1,nothing)
+Mul(a::Var, b::Var) = Var(a.data .* b.data, [a, b], Mul, nothing, max(a.generation, b.generation) + 1, nothing)
 
 #除法
 struct Div end
-Div(a::Var, b::Var) = Var(a.data ./ b.data, [a, b], Div, nothing, max(a.generation, b.generation) + 1,nothing)
+Div(a::Var, b::Var) = Var(a.data ./ b.data, [a, b], Div, nothing, max(a.generation, b.generation) + 1, nothing)
 
 #e^x函数
 struct Exp end
-Exp(x::Var) = Var(exp.(x.data), [x], Exp, nothing, x.generation + 1,nothing)
+Exp(x::Var) = Var(exp.(x.data), [x], Exp, nothing, x.generation + 1, nothing)
 
 #幂运算
 struct Pow end
-Pow(x::Var, c::Number) = Var(x.data .^ c, [x,c], Pow, nothing, x.generation + 1,nothing)
+Pow(x::Var, c::Number) = Var(x.data .^ c, [x, c], Pow, nothing, x.generation + 1, nothing)
 
 #sin函数
 struct Sin end
-Sin(x::Var) = Var(sin.(x.data), [x], Sin, nothing, x.generation + 1,nothing)
+Sin(x::Var) = Var(sin.(x.data), [x], Sin, nothing, x.generation + 1, nothing)
 
 #cos函数
 struct Cos end
-Cos(x::Var) = Var(cos.(x.data), [x], Cos, nothing, x.generation + 1,nothing)
+Cos(x::Var) = Var(cos.(x.data), [x], Cos, nothing, x.generation + 1, nothing)
+
+#tanh函数
+struct Tanh end
+Tanh(x::Var) = Var(tanh.(x.data), [x], Tanh, nothing, x.generation + 1, nothing)
+
+#reshape函数
+struct Reshape end
+Reshape(x::Var,shape) = Var(reshape(x.data,shape), [x], Reshape, nothing, x.generation + 1, nothing)
+
+#transpose函数
+struct Transpose end
+Transpose(x::Var) = Var(transpose(x.data), [x], Transpose, nothing, x.generation + 1, nothing)
+
+
 
 
 
@@ -59,17 +73,23 @@ function gradient(x::Var, y::Var)
     elseif y.creator === Mul
         return y.grad * (x === y.pre[1] ? (y.pre[2]) : (y.pre[1]))
     elseif y.creator === Div
-        return y.grad * (x === y.pre[1] ? (1 / y.pre[2]) : (y.pre[1] / y.pre[2] ^ 2) * -1)
+        return y.grad * (x === y.pre[1] ? (1 / y.pre[2]) : (y.pre[1] / y.pre[2]^2) * -1)
     elseif y.creator === Square
         return 2 * x * y.grad
     elseif y.creator === Exp
         return exp.x * y.grad
     elseif y.creator === Pow
-        return x^(y.pre[2]-1)*y.grad*y.pre[2]#y的父变量中1是底数2是幂c
+        return x^(y.pre[2] - 1) * y.grad * y.pre[2]#y的父变量中1是底数2是幂c
     elseif y.creator === Sin
         return cos(x) * y.grad
     elseif y.creator === Cos
         return -sin(x) * y.grad
+    elseif y.creator === Tanh
+        return y.grad * (1 - y * y)
+    elseif y.creator === Reshape
+        return reshape(y.grad,size(x.data))
+    elseif y.creator === Transpose
+        return transpose(y.grad)
     else
         return nothing
     end
@@ -85,7 +105,7 @@ function backward!(v::Var)
         y = list[index]#通过求generation最大值的方法确定辈分最小的会比排序快
         if y.pre !== nothing#如果有父变量
             for x in y.pre#对于每一个父变量
-                if typeof(x)!=Var
+                if typeof(x) != Var
                     continue
                 end#有父级包含非Var类的情况比如幂函数
                 gr = gradient(x, y)#储存该父变量的导数值
@@ -115,12 +135,23 @@ end
 -(a::Var, b::Var) = Add(a, Neg(b))
 -(a::Var, b::Number) = Add(a, Var([b * -1]))
 -(a::Number, b::Var) = Add(Var([a]), -b)
-*(a::Var, b::Var) = Mul(a,b)
+*(a::Var, b::Var) = Mul(a, b)
 *(a::Var, b::Number) = Mul(a, Var([b]))
 *(a::Number, b::Var) = Mul(Var([a]), b)
-/(a::Var, b::Var) = Div(a,b)
+/(a::Var, b::Var) = Div(a, b)
 /(a::Var, b::Number) = Div(a, Var([b]))
 /(a::Number, b::Var) = Div(Var([a]), b)
 ^(v::Var, n::Number) = Pow(v, n)
-sin(v::Var)=Sin(v)
-cos(v::Var)=Cos(v)
+sin(v::Var) = Sin(v)
+cos(v::Var) = Cos(v)
+tanh(v::Var) = Tanh(v)
+transpose(v::Var) = Transpose(v)
+function reshape(v::Var,shape)
+    if size(v.data)==shape
+        return v
+    else
+        return Reshape(v,shape)
+    end
+end
+
+
